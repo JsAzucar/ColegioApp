19c273ad155775508d377e95d14ad7581ab1a5b8
JsAzucar <ismaelazucaroct@gmail.com>
2025-03-26 10:39:35 -0600

    chore(project): reorganizar estructura y agregar configuración de Firebase
    Se reorganizó la estructura del proyecto para mayor orden y claridad.
    
    - Se crearon nuevas carpetas para organizar mejor el código.
    - Se agregó el archivo de configuración de Firebase.
    - Se instalaron y configuraron nuevos plugins y dependencias.

--- app/src/main/java/com/sugardaddy/colegioapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/sugardaddy/colegioapp/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.sugardaddy.colegioapp
+package com.sugardaddy.colegioapp.ui.main
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sugardaddy.colegioapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/sugardaddy/colegioapp/utils/ValidationUtils.kt
@@ -0,0 +1,4 @@
+package com.sugardaddy.colegioapp.utils
+
+object ValidationUtils {
+}
